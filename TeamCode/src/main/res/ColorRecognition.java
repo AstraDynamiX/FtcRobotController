0c8667d60fee174635c1fd121214e1a037bf37e7
mihneaoctalx-hue <mihnea.octalx@gmail.com>
2025-11-25 18:54:49 +0200

    Add files via upload

---
+++ TeamCode/src/main/res/ColorRecognition.java
@@ -0,0 +1,47 @@
+package org.firstinspires.ftc.teamcode.camera;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import org.firstinspires.ftc.robotcore.external.hardware.camera.WebcamName;
+import org.openftc.easyopencv.*;
+
+@TeleOp(name="Color Recognition")
+public class ColorRecognition extends LinearOpMode {
+
+    OpenCvWebcam webcam;
+    ColorPipeline pipeline;
+
+    @Override
+    public void runOpMode() {
+        int cameraMonitorViewId = hardwareMap.appContext.getResources()
+                .getIdentifier("cameraMonitorViewId","id",hardwareMap.appContext.getPackageName());
+
+        webcam = OpenCvCameraFactory.getInstance()
+                .createWebcam(hardwareMap.get(WebcamName.class,"Webcam 1"), cameraMonitorViewId);
+
+        pipeline = new ColorPipeline();
+        webcam.setPipeline(pipeline);
+
+        webcam.openCameraDeviceAsync(new OpenCvCamera.AsyncCameraOpenListener() {
+            @Override
+            public void onOpened() {
+                webcam.startStreaming(640,480, OpenCvCameraRotation.UPRIGHT);
+            }
+            @Override
+            public void onError(int errorCode) {}
+        });
+
+        telemetry.addLine("Camera ready. Waiting for start...");
+        telemetry.update();
+        waitForStart();
+
+        while (opModeIsActive()) {
+            telemetry.addData("Detected Color", pipeline.detectedColor);
+            telemetry.update();
+            sleep(20);
+        }
+
+        webcam.stopStreaming();
+    }
+}
